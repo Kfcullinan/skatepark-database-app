25df729a8dc8ce519aa87b45d7d75627dd032a75
Keith Cullinan <kfcullinan@gmail.com>
2022-10-16 14:55:16 -0500

    started database and installed dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import store from './redux/store';
-
+import logger from 'redux-logger';
 import App from './components/App/App';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+const storeInstance = createStore(
+  combineReducers({
+      
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger),
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
