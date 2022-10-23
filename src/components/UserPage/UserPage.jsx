875ba5bcd8d37e0e615f94074aa992936a82767a
Keith Cullinan <kfcullinan@gmail.com>
2022-10-23 10:59:23 -0500

    updated get all skateparks, added more to db

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector, useDispatch} from 'react-redux';
-import {useHistory} from 'react-router-dom';
-import {useEffect} from 'react';
-import axios from 'axios';
-
+import React from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   const history = useHistory();
   const dispatch = useDispatch();
-    const skateparks = useSelector(store => store.skateparks);
- 
-    useEffect(() => {
-     dispatch({ type: 'FETCH_SKATEPARKS' });
+  const skateparks = useSelector((store) => store.skateparks);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_SKATEPARKS" });
   }, []);
 
+  const detail = (movie) =>{
+    dispatch ({ type: 'SELECT_SKATEPARK', payload: skatepark});
+    dispatch ({ type: 'FETCH_FEATURES', payload: skatepark.id});
+    history.push('/details')
+  }
+  
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
     
-      <h2>Skateparks</h2>
-      <pre>{JSON.stringify(skateparks)}</pre>
-      {
-       skateparks.map(skateparks => {
-          return <div key={skateparks.id}>{skateparks.name}
-          <br></br>
-          {/* <button>DELETE</button> finish this */}
-          </div>
-          
-      })
-      }
-     </div>
+        <h1>Skateparks</h1>
+        <section>
+          <pre>{JSON.stringify(skateparks)}</pre>
+          {skateparks.map((skateparks) => {
+            return (
+              <div key={skateparks.id}>
+                <h2>{skateparks.name}</h2>
+                <h3>{skateparks.location}</h3>
+                <h3>{skateparks.space_type}</h3>
+                <h3>{skateparks.difficulty}</h3>
 
-    
+                <br></br>
+                {/* <button>DELETE</button> finish this */}
+              </div>
+            );
+          })}
+        </section>
+    </div>
   );
 }
 
